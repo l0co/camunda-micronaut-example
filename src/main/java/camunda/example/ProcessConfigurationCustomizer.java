b308819cd16932b74e6e8a8df9468a147d840d03
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-07-07 08:47:16 +0200

    enable auto assignment on start event extension

--- src/main/java/camunda/example/ProcessConfigurationCustomizer.java
+++ src/main/java/camunda/example/ProcessConfigurationCustomizer.java
@@ -6,16 +6,19 @@ import org.camunda.bpm.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
 import org.camunda.bpm.engine.impl.bpmn.parser.AbstractBpmnParseListener;
 import org.camunda.bpm.engine.impl.bpmn.parser.BpmnParseListener;
 import org.camunda.bpm.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.camunda.bpm.engine.impl.pvm.process.ActivityImpl;
 import org.camunda.bpm.engine.impl.pvm.process.ScopeImpl;
 import org.camunda.bpm.engine.impl.task.listener.ExpressionTaskListener;
 import org.camunda.bpm.engine.impl.util.xml.Element;
+import org.camunda.bpm.engine.impl.variable.VariableDeclaration;
 import org.camunda.bpm.engine.task.Task;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
@@ -33,6 +36,25 @@ public class ProcessConfigurationCustomizer implements Consumer<ProcessEngineCon
 		
 		list.add(new AbstractBpmnParseListener() {
 
+			protected boolean enableAutoAssignment = false;
+
+			@Override
+			public void parseStartEvent(Element startEventElement, ScopeImpl scope, ActivityImpl startEventActivity) {
+				super.parseStartEvent(startEventElement, scope, startEventActivity);
+
+				Optional.ofNullable(startEventElement.element("extensionElements"))
+					.map(extensionElements -> extensionElements.element("properties"))
+					.ifPresent(properties -> properties.elements().forEach(property -> {
+
+						// enable lane auto assignment
+						if ("assignment".equals(property.attribute("name"))
+								&& "auto".equals(property.attribute("value")))
+							enableAutoAssignment = true;
+
+					}));
+
+			}
+
 			/**
 			 * To each use activity we add on task create call to {@link ProcessService#tryAutoAssignTask(Task)} to try auto assign task.
 			 */
@@ -40,15 +62,17 @@ public class ProcessConfigurationCustomizer implements Consumer<ProcessEngineCon
 			public void parseUserTask(Element userTaskElement, ScopeImpl scope, ActivityImpl activity) {
 				super.parseUserTask(userTaskElement, scope, activity);
 
-				((UserTaskActivityBehavior) activity.getActivityBehavior()).getTaskDecorator().getTaskDefinition()
-					.addTaskListener(TaskListener.EVENTNAME_CREATE, new ExpressionTaskListener(
-					config.getExpressionManager().createExpression("${processService.tryAutoAssignTask(task)}")));
+				if (enableAutoAssignment) {
+					((UserTaskActivityBehavior) activity.getActivityBehavior()).getTaskDecorator().getTaskDefinition()
+						.addTaskListener(TaskListener.EVENTNAME_CREATE, new ExpressionTaskListener(
+							config.getExpressionManager().createExpression("${processService.tryAutoAssignTask(task)}")));
+				}
 
 			}
 
 		});
 
-		config.setCustomPostBPMNParseListeners(list);
+		config.setCustomPreBPMNParseListeners(list);
 	}
 
 }
